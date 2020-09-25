ddcbe64ea6590a5d2d3bd4ea792fff4430240e13
Wilson Estrada <wilson.estrada@gmail.com>
2020-09-24 22:12:16 -0500

    este es un commit nuevo

---
+++ ejemplo1/js/operaciones.js
@@ -0,0 +1,14 @@
+function operaciones{
+    let valorZapatos=0;
+    let valorTennis=0;
+    let valorCamiseta=0;
+    let valorJean=0;
+    let costoTotal=0;
+    let promedio=0;
+    let nuevoJeans=0;
+    let nuevoZapatos=0;
+
+    valorzapatos=parseInt(document.getElementById().value);
+
+
+}
